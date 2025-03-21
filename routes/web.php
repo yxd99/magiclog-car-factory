c65f93c0e0774e6cb03f35787081a104f00b9cad
Yesid Hernandez <fyesid.h@gmail.com>
2025-03-21 14:29:24 -0500

    feat(routes): Update fallback route to redirect to Car management

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;use App\Http\Controllers\UsersController;
  
 Route::resource('/cars', App\Http\Controllers\CarController::class);
-Route::get('/', function () {
-    return redirect('/cars'); 
+Route::fallback(function () {
+    return redirect('/cars');
 });
